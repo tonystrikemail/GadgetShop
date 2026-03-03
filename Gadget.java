870847f9f050ca1e715fae3842dde84210194e8f
Tony Strike <tonystrikemail@gmail.com>
2026-03-03 12:21:35 +0000

    Start of the project from beggining deadline in tree weeks

---
+++ Gadget.java
@@ -0,0 +1,32 @@
+
+
+
+
+
+public class Gadget {
+// Atributes:
+private final String model; //create model variable
+private final double price; //create price variable
+private final int weight; //create weight variable
+private final String size; //create size variable
+
+// The constructor:
+public Gadget(String model, double price, int weight, String size){
+
+// Initialise constructor fields to the attributes    
+this.model= model;
+this.price= price;
+this.weight= weight;
+this.size= size; 
+
+}
+
+
+
+
+
+
+
+
+
+}
