9c3221cda1985dbcf12e57788496628d383741bd
Tony Strike <tonystrikemail@gmail.com>
2026-03-16 16:24:55 +0000

    Created some of the labels and text fields with easy alligment and clean the code

--- GadgetShop.java
+++ GadgetShop.java
@@ -17,19 +17,61 @@ import java.util.ArrayList;
 
 public class GadgetShop extends Application
 {
+    // array list containing all the gadgets in our shop
+    private final ArrayList<Gadget> gadgets = new ArrayList<>();
+
+    // log area displang output messages
     private final TextArea logArea = new TextArea();
+    // set fixed width of 200px for all fields
+    private static final int FIELD_WIDTH = 200;
+    // set fixed 
+    // create a method for creating labels with fields and add them to the pane
+    private TextField createFieldWithLabel(String text, int labelX, int labelY, int fieldX, Pane root){
+        //label object
+        Label label = new Label(text);
+        label.setLayoutX(labelX);
+        label.setLayoutY(labelY);
+        //field oject
+        TextField field = new TextField();
+        field.setLayoutX(fieldX);
+        field.setLayoutY(labelY);
+        field.setPrefWidth(FIELD_WIDTH);//same width to all fields
+        //add the elements to the pane
+        root.getChildren().addAll(label,field);
+        return field;// returns the field so that we can read data
+    }
 
     @Override
     public void start(Stage stage){
-        
-        // create the window container for all the fields, buttons and info
+
         Pane root = new Pane();
+        // variables for the coordinates of the fields and labels for quick alligment
+        int labelX = 20;
+        int fieldX = 100;
+        int startY = 20;
+        int offset = 35;
+
+        // Lables-------------
+        TextField modelField = createFieldWithLabel("Model: ",labelX, startY,fieldX, root);
+        TextField priceField = createFieldWithLabel("Price: ",labelX, startY + offset, fieldX,root);
+        TextField weightField = createFieldWithLabel("Weight: ",labelX, startY + offset*2, fieldX,root);
+        TextField sizeField = createFieldWithLabel("Size: ",labelX, startY + offset*3, fieldX,root);
+
+        root.getChildren().addAll(
+                   logArea);
+
+        logArea.setLayoutX(20);
+        logArea.setLayoutY(360);
+        logArea.setPrefWidth(500);
+        logArea.setPrefHeight(230);
+
+        // creates the window container for all the fields, buttons and info
+
         stage.setScene(new Scene(root,600,800));
         stage.setTitle("Tony's Gadget Shop");
         stage.show();
     }
 
-    
     // start JavaFX application
     public static void main(String[] args){
         launch();
