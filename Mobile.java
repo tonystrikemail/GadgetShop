d448336885017b5dd71c953dca946d15cb11d47c
Tony Strike <tonystrikemail@gmail.com>
2026-03-16 12:35:41 +0000

    Basic structure of Mobile class

--- Mobile.java
+++ Mobile.java
@@ -12,7 +12,18 @@ public class Mobile extends Gadget
 {
     // the attribute of Mobile gadgets
     private int callingCredit;
-    
+
     // constructor
-    
+    public Mobile(String model, double price, int weight, String size, int callingCredit){
+        //takes the attributes from super class to initialise
+        super(model, price, weight, size);
+
+        //initializing its own attributes
+        this.callingCredit = callingCredit;
+    }
+    // assessor method
+    public int getCallingCredit(){
+        return callingCredit;
+    }
+
 }
